8e95c377e6a07f38cacb5c4134bb198f8026ffe3
Evan Labourdette <evan.labourdette@epitech.eu>
2024-07-02 16:20:47 +0200

    feat(Update next js project): Update next js project

--- docroadmap-extension/src/app/page.tsx
+++ docroadmap-extension/src/app/page.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-red-400 p-4">
       <h1 className="text-4xl font-bold mb-4 text-center">
         Bienvenue à DocRoadmap
       </h1>
