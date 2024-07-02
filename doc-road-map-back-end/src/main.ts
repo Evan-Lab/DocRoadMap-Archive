2e433392c32c3b9c6c0d4649b8f48e07f9fa367b
BaptistePer <baptiste.perard@epitech.eu>
2024-07-02 17:00:39 +0200

    feat(Swagger): Adding Swagger

--- doc-road-map-back-end/src/main.ts
+++ doc-road-map-back-end/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Doc RoadMap API')
+    .setDescription('The Doc RoadMap API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
